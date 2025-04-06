c8198f04722ba937f0db30c45097c9b00fc017e8
Ec <correo@email.com>
2025-04-05 22:45:46 -0400

    Agregando cambios temporales a Github

--- user-service/src/routes/userRoutes.js
+++ user-service/src/routes/userRoutes.js
@@ -3,8 +3,15 @@ const { registerUser, loginUser } = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/register', registerUser);
-router.post('/login', loginUser);
+//router.post('/register', registerUser);
+//router.post('/login', loginUser);
+
 // Add more routes for getting user, updating user, etc.
+const userController = require('../controllers/userController');
+
+router.post('/register', userController.registerUser);
+router.post('/login', userController.loginUser);
+router.put('/:username', userController.updateUserProfile);
+
 
 module.exports = router;
